48d8d2cb064cdfc777e3829983fe4f6a6608a951
jwhector <jared.hector@gmail.com>
2021-10-28 16:45:26 -0700

    add category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,57 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({ include: Product })
+    .then(categories => {
+      const categoriesJSON = categories.map(category => category.toJSON());
+      res.status(200).json(categories);
+    })
+    .catch(err => {
+      res.status(500).send('The server encountered an error.');
+    })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({ where: { id: req.params.id }, include: Product})
+    .then(category => {
+      res.status(200).json(category.toJSON());
+    })
+    .catch(err => {
+      res.status(500).send('The server encountered an error.');
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then(created => {
+      res.status(200).json(created.toJSON());
+    })
+    .catch(err => {
+      res.status(500).send('An error occurred.');
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, { where: { id: req.params.id } })
+    .then(updated => {
+      res.status(200).json(updated.toJSON());
+    }).catch(err => {
+      res.status(500).send('An error occurred.');
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({ where: { id: req.params.id } })
+    .then(destroyed => {
+      res.status(200).json(destroyed);
+    }).catch(err => {
+      res.status(500).send('An error occurred.');
+    });
 });
 
 module.exports = router;
